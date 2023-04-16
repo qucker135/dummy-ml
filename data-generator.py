c4389d688e326094ccdaf49f2dd3f94c4688c027
bmatysiak <bartosz.matysiak@pwr.edu.pl>
2023-04-16 15:17:33 +0200

    Create data generator

---
+++ data-generator.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def label(data: np.ndarray):
+    values = []
+    for sample in data:
+        if sample[0] > 0.3 and sample[1] > 0.3:
+            values.append(1.0)
+        elif sample[1] > 0.5:
+            values.append(3.0)
+        elif sample[0] > 0.6:
+            values.append(4.0)
+        else:
+            values.append(2.0)
+    
+    return np.insert(data.copy(), data.shape[1], values, axis=1)
+
+unlabeled_data = np.random.uniform(0.0, 1.0, (100,2))
+#print(unlabeled_data)
+labeled_data = label(unlabeled_data)
+print(labeled_data)
+
+np.savetxt("data.csv", labeled_data, delimiter=",")
