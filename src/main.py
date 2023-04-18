ae87d1ff4f24d2fb7ef8c98c9a0a3a798ceaaf6b
qucker135 <qucker135@gmail.com>
2023-04-18 21:19:19 +0200

    Start creating neural network training

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
 import numpy as np
-import activations as ac
+from activations import *
+from layers import *
 
-sa = ac.SigmoidActivation()
-print(sa.compute(np.array([0.0, 0.1, 1.0])))
+if __name__ == "__main__":
+    training_data = np.loadtxt("data.csv", delimiter=",")
+
+    model = [
+        LinearLayer(2, 3, SigmoidActivation()),
+        LinearLayer(3, 3, SigmoidActivation()),
+        LinearLayer(3, 4, SigmoidActivation())
+    ]
+
+    for sample in training_data:
+        data_in = sample[0:2]
+        label = int(sample[2])
+        expected_activation = np.identity(4)[label - 1]
+
+        inp_for_next_layer = data_in.copy()
+        for layer in model:
+            layer.forward(inp_for_next_layer)
+            inp_for_next_layer = layer.output.copy()
+
+        grad = 2.0 * (model[-1].output - expected_activation)
+        for layer in reversed(model):
+            grad = layer.backpropagate(grad)
+
+        ###
